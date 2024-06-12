570d462961aa63f87b045462f39e60691b4b0e4b
It-team-insta <itteaminstaict@gmail.com>
2024-06-12 10:44:45 +0000

    discussion on conversion operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = "33"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+//when we convert string 33 in number it easily converted in number
+//if we convert text String in number its datatype is changed in number
+// boolean true will convert in 1 and false in 0
+
+// 33 => 33
+// 33Banana => Nan
+//null => 0,
+// boolean => 0/1
+
+// let someNumber = 22
+
+// let stringNumber = String(someNumber)
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// ************************ Operations ********************
+console.log(
+    "**********************Operations*********************"
+);
+let value = 3
+
+let negValue = -value
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Bhayaa"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);//ans 12
+console.log("1"+2+2); //ans 2
+console.log( 1 + 2 + "2" ); // ans 32
+//https://tc39.es/ecma262/multipage/abstract-operations.html
