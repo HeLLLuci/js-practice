1ccdf144d999928f926cb3bf141e7b9b33de8e59
It-team-insta <itteaminstaict@gmail.com>
2024-06-12 10:21:20 +0000

    datatypes Discussed

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "khandodon@gmail.accountEmail"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+// accountId = 2
+// console.log(accountId); //not allowed as conts keywords says value cannot changed further
+
+accountEmail = "PappuPailwan@gmail.com"
+accountPassword = "123123"
+accountCity = "Kothrud"
+
+console.table([
+    accountId, accountEmail, accountPassword, accountCity, accountState
+]); 
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as new version
+
+//alert("This is alert message") //We are using Node.js not browser that's why it will not run.
+
+// console.log("First Message"); console.log("Second line") //this line will be executed but will is least readable, and a good programer must always be readable.
+
+//there are two main documentations for javascript which are MDN and TC3(ECMA) which provides standards for language engines to define what should do what.
+
+
+let name = "Pappu" //string datatype
+let age = 15 //number
+let isLoading = false
+
+// number => 2 to power 53
+//bigint => big number only
+//string => " "
+//boolean => true, false
+//null => standalone value representation of empty value
+//undefined => when you don't define the value.
+//symbol => identifier for components mostly used in react, tools like figma uses symbol alot to represent unique components.
+//object => 
+
+//console.log(typeof null); //object
+//console.log(typeof undefined) //undefined
