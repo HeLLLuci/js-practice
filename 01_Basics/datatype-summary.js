ae8d63c569f59c1e01779f1f173e5203d1e043b1
LuCi <111504296+HeLLLuci@users.noreply.github.com>
2024-06-13 09:05:19 +0000

    datatypes summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,27 @@
+//call by value and call by reference
+
+// primitive datatypes:
+// 7 (String, Number, Boolean, null, undefined, Symbol(used to make any value unique), BigInt) //call by value
+
+// JS Dynamically typed language: means we dont have to deifne type of any value.
+
+//Reference types (Non-primitive types):
+// Arrays, Objects, Enums, Functions
+
+
+// const heros = ['Shaktiman', 'Naagraj', 'Doga'] //return type: object
+
+// console.log(typeof heros);
+
+// let myOj ={
+//     name: 'hitesh',
+//     age: 22,
+// } //return type: object
+
+// console.log(typeof myOj);
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// } //return type: function
+
+// console.log(typeof myFunction);
