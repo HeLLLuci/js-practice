a8a30f0234a63dc211f3e52dd4a924244b5ec5ac
LuCi <111504296+HeLLLuci@users.noreply.github.com>
2024-06-13 08:38:30 +0000

    datatype comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+
+
+// console.log("2">1);
+
+// console.log(null > 0); //false
+// console.log(null==0);  //false
+// console.log(null>=0);//it converted null into zero //true
+
+// console.log(undefined == 0); //false
+// console.log(undefined <= 0); //false
+// console.log(undefined >= 0); //false
+
+/// === strict check, not only check only value but also check datatype of numbers being ccompared
