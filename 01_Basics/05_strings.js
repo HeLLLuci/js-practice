2c563dfee12bb0b44655722efd413de58c1497db
LuCi <111504296+HeLLLuci@users.noreply.github.com>
2024-06-13 15:45:35 +0530

    Operations on strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -4,3 +4,4 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //String intepolation
 
 const gameName = new String("Game");
+// to check operations on string you can either navigate to MDN documentation ror just go to any broswer then inspect and create new string object and call that object youll get the operations on string.
