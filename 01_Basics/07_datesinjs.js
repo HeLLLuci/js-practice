b19ac46e24e00b272a71ca1abb5628fb2ff31222
LuCi <111504296+HeLLLuci@users.noreply.github.com>
2024-06-17 08:49:32 +0000

    Dates Discussion

---
+++ 01_Basics/07_datesinjs.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+
+let myCreatedDate = new Date(2023, 0, 23) //months starting from zero in javascript
+// let myNewDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+
+let timeStamp = new Date();
+console.log(timeStamp.getDate());
