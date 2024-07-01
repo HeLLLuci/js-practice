3bdb9a4a337e2bd6224514e25c7b36b0104af780
LuCi <111504296+HeLLLuci@users.noreply.github.com>
2024-07-01 07:09:04 +0000

    Completed arrays part 1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,43 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ["Iron Man", "captain America", "Hawkeye", "Hulk", "Thor"]
+
+const myArr2 = new Array(0,1,2,3,4,5)
+
+console.log(myArr[0]);
+
+//methods
+
+
+// myArr.push(6) //To push new elements at the last of the array.
+// console.log(myArr);
+
+// myArr.pop(); //To pop the last element in the array.
+
+// myArr.unshift(9) //To Add element at the first position of the array.
+
+// myArr.shift(); //To remove the element at the first position of the array.
+
+// console.log(myArr.includes(9)); //To check whether perticular element is in the array.
+// console.log(myArr.indexOf(3)); //TO get the index of the perticular element in the array.
+
+// const newArray = myArr.join() // Converts the array into string in format of comma seperated
+
+// console.log(myArr);
+
+//Slice and Splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3) //To take the portion of array mentioned in start and end index (excludes end index).
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1, 3)  //To take the portion of array mentioned in start index and end index (includes the end index) (NOTE: it manipulates original array and remove the spliced array from orignal array)
+// console.log(myn2);
+// console.log("C ", myArr);
+
+
